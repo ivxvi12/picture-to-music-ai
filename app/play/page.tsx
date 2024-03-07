4eeea123047fef3825f79d7cf46f60e73f1cc3f0
axelgcd <axelguichard.45@gmail.com>
2024-03-07 05:02:45 +0100

    feat(app) play song from url

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -2,11 +2,35 @@
 import { Button } from "@/components/ui/button";
 import { Pause, Play, SkipBack } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function UploadPictures() {
-  const songs = [{ name: "Song 1" }, { name: "Song 2" }, { name: "Song 3" }];
+  const [songs, setSongs] = useState([] as string[]);
   const [playingSong, setPlayingSong] = useState(String);
+  const [audio] = useState(new Audio());
+  const fetchSongs = async () => {
+    const response = await fetch("http://52.141.27.205:8000/music/", {
+      method: "GET",
+      headers: {},
+    });
+    const data = await response.json();
+    const song_url = data.url as string;
+    setSongs([...songs, song_url]);
+  };
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+
+  const handlePlaying = (song_url: string) => {
+    if (playingSong === song_url) {
+      setPlayingSong("");
+      audio.pause();
+    } else {
+      setPlayingSong(song_url);
+      audio.src = song_url;
+      audio.play();
+    }
+  };
 
   return (
     <main className="flex min-h-screen flex-col p-7">
@@ -15,20 +39,16 @@ export default function UploadPictures() {
       </Link>
       <div className="flex flex-col items-center">
         <h1 className="text-4xl mt-32 mb-24">Play...</h1>
-        <div className="flex flex-col">
+        <div className="flex flex-col mb-4">
           {songs.map((song) => (
-            <div className="flex flex-row px-4 py-6 border rounded-md mb-4 space-x-6">
+            <div className="flex flex-row px-4 py-6 border rounded-md mb-6 space-x-6" key={song}>
               <div className="rounded-full bg-black w-10 h-10"></div>
               <div className="flex flex-col items-center">
                 <div
                   onClick={() => {
-                    if (playingSong === song.name) {
-                      setPlayingSong("");
-                    } else {
-                      setPlayingSong(song.name);
-                    }
+                    handlePlaying(song);
                   }}>
-                  {playingSong === song.name ? (
+                  {playingSong === song ? (
                     <Pause size={24} className="text-black mb-4" />
                   ) : (
                     <Play size={24} className="text-black mb-4" />
